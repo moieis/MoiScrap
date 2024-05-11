35953977454ade3e5b53d3ebd7aa0bc1e2b909bd
moieis <87827220+moieis@users.noreply.github.com>
2024-05-11 13:54:05 +0200

    Update y.py

--- y.py
+++ y.py
@@ -39,12 +39,12 @@ def man():
     
     
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=r'/app/.chrome-for-testing/chromedriver-linux64/chromedriver', chrome_options=chrome_options)
     
     driver.get("https://www.gemeentebest.nl/nieuws/")
     time.sleep(2)
