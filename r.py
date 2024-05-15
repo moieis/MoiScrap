922217c8e08c0cdb37469a60bfe5fd49f63bf7a0
moieis <87827220+moieis@users.noreply.github.com>
2024-05-15 04:08:17 +0200

    Update r.py

--- r.py
+++ r.py
@@ -53,7 +53,7 @@ def man():
     options = webdriver.ChromeOptions() 
     options.headless = True
     options.add_argument("start-maximized")
-    option.add_argument("--no-sandbox")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
